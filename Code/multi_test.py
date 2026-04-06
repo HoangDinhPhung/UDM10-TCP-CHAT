8390882056e4f731b77bb312abd7018f7a64c669
Hoang Dinh Phung <phunghd7188@ut.edu.vn>
2026-04-06 13:47:08 +0700

    Add E3_WebSocket feature

---
+++ Code/multi_test.py
@@ -0,0 +1,15 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+def client():
+    s = socket.socket()
+    s.connect((HOST, PORT))
+    s.sendall(b"hello")
+    print(s.recv(1024))
+    s.close()
+
+for _ in range(20):
+    threading.Thread(target=client).start()
