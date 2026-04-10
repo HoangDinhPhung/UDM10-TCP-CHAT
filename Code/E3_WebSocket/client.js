7d91f12add3e72355283f27cdc3313906a288088
Hoang Dinh Phung <phunghd7188@ut.edu.vn>
2026-04-11 00:35:41 +0700

    Phung update E2 + NaTthanh complete E3 WebSocket chat system

---
+++ Code/E3_WebSocket/client.js
@@ -0,0 +1,23 @@
+let ws = new WebSocket("ws://127.0.0.1:5000");
+
+let chat = document.getElementById("chat");
+
+ws.onopen = () => {
+    chat.innerHTML += "<p>[Connected]</p>";
+};
+
+ws.onmessage = (event) => {
+    chat.innerHTML += "<p>" + event.data + "</p>";
+};
+
+function sendMsg() {
+    let input = document.getElementById("msg");
+    let msg = input.value;
+
+    if (msg.trim() === "") return;
+
+    ws.send(msg);
+    chat.innerHTML += "<p><b>You:</b> " + msg + "</p>";
+
+    input.value = "";
+}
