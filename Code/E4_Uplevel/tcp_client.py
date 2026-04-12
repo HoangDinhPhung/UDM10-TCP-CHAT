4d5a24965f2a847885e029b7e127005286ef5204
Hoàng Đình Phùng 064205007188 <phunghd7188@ut.edu.vn>
2026-04-12 22:43:18 +0700

    E4-giao dien chat

--- Code/E4_Uplevel/tcp_client.py
+++ Code/E4_Uplevel/tcp_client.py
@@ -1,1 +1,25 @@
+import socket
+import threading
 
+HOST = "127.0.0.1"
+PORT = 8765
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+def receive():
+    while True:
+        try:
+            msg = client.recv(1024).decode()
+            print("\nServer:", msg)
+        except:
+            print("Mat ket noi server")
+            break
+
+def send():
+    while True:
+        msg = input("Nhap tin nhan: ")
+        client.send(msg.encode())
+
+threading.Thread(target=receive).start()
+threading.Thread(target=send).start()
